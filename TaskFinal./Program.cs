e5d247b7d15df223f814720cfea7f8ed38b6b879
Сергей Ваев <99madagascar@mail.ru>
2023-03-26 14:37:06 +0300

    написал код задачи

--- TaskFinal./Program.cs
+++ TaskFinal./Program.cs
@@ -6,3 +6,36 @@
 // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 // ['1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
+
+
+
+
+
+        // Ввод массива строк с клавиатуры
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string[]ArrayInput = Console.ReadLine().Split(',');
+
+        // Создание нового массива с длиной не более 3 символов
+        string[] ArrayOutput = new string[ArrayInput.Length];
+        int count = 0;
+        for (int i = 0; i < ArrayInput.Length; i++) 
+        {
+            if (ArrayInput[i].Length <= 3) 
+            {
+                ArrayOutput[count] = ArrayInput[i];
+                count++;
+            }
+        }
+
+        // Вывод нового массива на экран
+        Console.WriteLine("Новый массив:");
+    
+        for (int i = 0; i < count; i++) 
+        {
+            Console.Write(ArrayOutput[i]+". ");
+        }
+
+        Console.ReadKey();
+
+
+
